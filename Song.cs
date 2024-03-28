36352f69667548740e4aeef022636979722e8e06
nedoprogramist89 <keksdam666@gmail.com>
2024-03-28 19:40:40 +0300

    Initial commit

---
+++ Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using TagLib;
+
+namespace PracticalWork3
+{
+    internal class Song
+    {
+        public string path { get; set; }
+        public TagLib.File file { get; set; }
+        public string songName { get; set; }
+
+        public Song(string path)
+        {
+            this.path = path;
+            file = TagLib.File.Create(path);
+            songName = file.Tag.Title ?? Path.GetFileNameWithoutExtension(path);
+        }
+    }
+}
